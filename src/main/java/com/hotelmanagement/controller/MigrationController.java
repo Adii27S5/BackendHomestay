0b8762e2c3c109551536bd174323d5b4aefb4a9a
Adii27S5 <adityasingh01227@gmail.com>
2026-04-28 09:56:05 +0530

    Add more admin debug links for all database tables

--- src/main/java/com/hotelmanagement/controller/MigrationController.java
+++ src/main/java/com/hotelmanagement/controller/MigrationController.java
@@ -16,9 +16,33 @@ public class MigrationController {
     @Autowired
     private com.hotelmanagement.repository.AppUserRepository userRepository;
 
+    @Autowired
+    private com.hotelmanagement.repository.HomestayRepository homestayRepository;
+
+    @Autowired
+    private com.hotelmanagement.repository.AttractionRepository attractionRepository;
+
+    @Autowired
+    private com.hotelmanagement.repository.HotelRepository hotelRepository;
+
     @GetMapping("/users")
     public ResponseEntity<java.util.List<com.hotelmanagement.model.AppUser>> getAllUsers() {
         return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    @GetMapping("/homestays")
+    public ResponseEntity<java.util.List<com.hotelmanagement.model.Homestay>> getAllHomestays() {
+        return ResponseEntity.ok(homestayRepository.findAll());
+    }
+
+    @GetMapping("/tours")
+    public ResponseEntity<java.util.List<com.hotelmanagement.model.Attraction>> getAllTours() {
+        return ResponseEntity.ok(attractionRepository.findAll());
+    }
+
+    @GetMapping("/hotels")
+    public ResponseEntity<java.util.List<com.hotelmanagement.model.Hotel>> getAllHotels() {
+        return ResponseEntity.ok(hotelRepository.findAll());
     }
 
     @GetMapping("/sync-bookings")
